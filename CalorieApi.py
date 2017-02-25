cc12cede713ad66db3c5aa88fe9e5f1b87aef246
Ishwar Agarwal <a.isshwar@gmail.com>
2017-02-25 12:43:50 -0500

    create class for calorie api

--- CalorieApi.py
+++ CalorieApi.py
@@ -2,28 +2,39 @@ import requests
 import json
 from util import load_config
 
-def getCal(query, cfg):
-	""" given a query returns an array of food dictionaries
-	return: [
-						{
-							"food_name":
-							"food_cal":
-						}
-					]
-	"""
-	endpoint = "natural/nutrients"
-	url = cfg["nutritionix"]["url"] + "/" + cfg["nutritionix"]["version"] + "/" + endpoint
-	headers = {'content-type': "application/json",
-					'x-app-id': cfg["nutritionix"]["appId"],
-					'x-app-key': cfg["nutritionix"]["appKey"],
-					'x-remote-user-id': "0" }
-	body = {"query": query, "timezone": "US/Eastern"}
+class CalorieCal:
+	def __init__(self, cfg):
+		self.appId = cfg["nutritionix"]["appId"]
+		self.appKey = cfg["nutritionix"]["appKey"]
+		self.baseurl = cfg["nutritionix"]["url"] 
+		self.version = cfg["nutritionix"]["version"]
+		self.headers = {'content-type': "application/json"}
 
-	url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+	def buildUrl(self, endpoint):
+		return self.baseurl + "/" + self.version + "/" + endpoint
+	
+	def authHeaders(self):
+		self.headers['x-app-id'] = self.appId
+		self.headers['x-app-key'] = self.appKey
+		self.headers['x-remote-user-id'] = "0"
 
-	print requests.post(url, data=json.dumps(body), headers=headers).text
+	def getCal(self, query):
+		""" given a query returns an array of food dictionaries
+		return: [
+							{
+								"food_name":
+								"food_cal":
+							}
+						]
+		"""
+		endpoint = "natural/nutrients"
+		url = self.buildUrl(endpoint)
+		self.authHeaders()
+		body = {"query": query, "timezone": "US/Eastern"}
+		print requests.post(url, data=json.dumps(body), headers=self.headers).text
 
 
 if __name__ == "__main__":
 	cfg = load_config()
-	getCal("1 Veggie Burger", cfg)
+	CC = CalorieCal(cfg)
+	CC.getCal("1 Veggie Burger")
