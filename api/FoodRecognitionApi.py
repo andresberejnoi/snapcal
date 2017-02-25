c2a19a35762534b2f2435942cc3ccfe5d39b9564
Ishwar Agarwal <a.isshwar@gmail.com>
2017-02-25 13:03:26 -0500

    project structure reorganization

--- api/FoodRecognitionApi.py
+++ api/FoodRecognitionApi.py
@@ -1,8 +1,10 @@
 import tensorflow as tf, sys
 
-class ImageRecognition:
+class FoodRecognition:
+	
 	def __init__(self):
 		self.image_path = None
+		self.label_lines = self.read_labels()
 	
 	def set_image_path(self, image_path):
 		self.image_path = image_path
@@ -17,6 +19,12 @@ class ImageRecognition:
 		label_lines = [line.rstrip() for line in tf.gfile.GFile("retrained_labels.txt")]
 		return label_lines
 	
+	def print_rank(prediction):
+		for node_id in prediction:
+			human_string = self.label_lines[node_id]
+			score = predictions[0][node_id]
+			print('%s (score = %.5f)' % (human_string, score))
+
 	def predict(self):
 		# Unpersists graph from file
 		with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
@@ -32,16 +40,15 @@ class ImageRecognition:
 			
 			# Sort to show labels of first prediction in order of confidence
 			top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-		return top_k
+		
+		label = self.label_lines[top_k[0]]
+		#certainty = predictions[0][top_k[0]]
+		
+		return label
     
 
-"""for node_id in top_k:
-				human_string = label_lines[node_id]
-				score = predictions[0][node_id]
-        print('%s (score = %.5f)' % (human_string, score))"""
-
 if __name__ == "__main__":
-	imgRec=ImageRecognition()
+	imgRec=FoodRecognition()
 	imgRec.set_image_path("brocolli.jpg")
 	print	imgRec.predict()
 	
